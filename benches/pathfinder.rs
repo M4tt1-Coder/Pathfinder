35e98793ee2463cc06a60aa6f4e52b175bf4f717
M4tt1-Coder <geimat75@gmail.com>
2025-11-25 00:23:46 +0100

    [ feature/benchmarks ] Undirected & Directed graphs benchmarks

--- benches/pathfinder.rs
+++ benches/pathfinder.rs
@@ -5,7 +5,7 @@ use pathfinder::{
     algorithms::{algorithm::Algorithm, dijkstra::DijkstraAlgorithm},
     graphs::{
         directed::{DirectedEdge, DirectedGraph},
-        graph::{Graph, Node},
+        graph::Node,
         undirected::{UndirectedEdge, UndirectedGraph},
     },
 };
@@ -13,126 +13,6 @@ use pathfinder::{
 fn main() {
     divan::main();
 }
-
-// ----- Benchmark the 'DirectedGraph' -----
-
-#[bench]
-fn create_directed_graph() {
-    let _ = DirectedGraph::default();
-}
-
-#[bench]
-fn add_edge_to_directed_graph() {
-    let mut graph = DirectedGraph::default();
-    graph.insert_node(Node::new("A".to_string()));
-    graph.insert_node(Node::new("B".to_string()));
-    graph.insert_edge(DirectedEdge::new(
-        Node::new("A".to_string()),
-        Node::new("B".to_string()),
-        5,
-    ));
-}
-
-#[bench]
-fn get_all_nodes_from_directed_graph(bencher: Bencher) {
-    bencher
-        .with_inputs(|| {
-            let mut graph = DirectedGraph::default();
-            graph.insert_node(Node::new("A".to_string()));
-            graph.insert_node(Node::new("B".to_string()));
-            graph.insert_edge(DirectedEdge::new(
-                Node::new("A".to_string()),
-                Node::new("B".to_string()),
-                5,
-            ));
-            graph
-        })
-        .bench_refs(|dg| {
-            let _nodes = dg.get_all_nodes();
-        });
-}
-
-#[bench]
-fn get_neighbors_from_directed_graph(bencher: Bencher) {
-    bencher
-        .with_inputs(|| {
-            let mut graph = DirectedGraph::default();
-            graph.insert_node(Node::new("A".to_string()));
-            graph.insert_node(Node::new("B".to_string()));
-            graph.insert_edge(DirectedEdge::new(
-                Node::new("A".to_string()),
-                Node::new("B".to_string()),
-                5,
-            ));
-            graph
-        })
-        .bench_refs(|dg| {
-            let _neighbors = dg.neighbors(&Node::new("A".to_string()));
-        });
-}
-
-#[bench]
-fn does_edge_already_exist_in_directed_graph(bencher: Bencher) {
-    bencher
-        .with_inputs(|| {
-            let mut graph = DirectedGraph::default();
-            graph.insert_node(Node::new("A".to_string()));
-            graph.insert_node(Node::new("B".to_string()));
-            graph.insert_edge(DirectedEdge::new(
-                Node::new("A".to_string()),
-                Node::new("B".to_string()),
-                5,
-            ));
-            graph
-        })
-        .bench_refs(|dg| {
-            let _exists = dg.does_edge_already_exist(&DirectedEdge::new(
-                Node::new("A".to_string()),
-                Node::new("B".to_string()),
-                5,
-            ));
-        });
-}
-
-#[bench]
-fn does_node_already_exist_in_directed_graph(bencher: Bencher) {
-    bencher
-        .with_inputs(|| {
-            let mut graph = DirectedGraph::default();
-            graph.insert_node(Node::new("A".to_string()));
-            graph.insert_node(Node::new("B".to_string()));
-            graph.insert_edge(DirectedEdge::new(
-                Node::new("A".to_string()),
-                Node::new("B".to_string()),
-                5,
-            ));
-            graph
-        })
-        .bench_refs(|dg| {
-            let _exists = dg.does_node_already_exist(&Node::new("A".to_string()));
-        });
-}
-
-#[bench]
-fn get_node_by_id_from_directed_graph(bencher: Bencher) {
-    bencher
-        .with_inputs(|| {
-            let mut graph = DirectedGraph::default();
-            graph.insert_node(Node::new("A".to_string()));
-            graph.insert_node(Node::new("B".to_string()));
-            graph.insert_edge(DirectedEdge::new(
-                Node::new("A".to_string()),
-                Node::new("B".to_string()),
-                5,
-            ));
-            graph
-        })
-        .bench_refs(|dg| {
-            let _node = dg.get_node_by_id("A");
-        });
-}
-
-// ----- Benchmark the 'UndirectedGraph' -----
 
 // ----- Benchmark the 'DijkstraAlgorithm' struct -----
 
